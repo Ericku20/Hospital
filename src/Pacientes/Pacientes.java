91519b31d558f2a322a31c28d6f924f7aa9f07a8
Erick <erickalfaro119@gmail.com>
2023-11-28 08:22:13 -0600

    Clase Pacientes
    Se crea la clase pacientes y se inicializa el repositorio

---
+++ src/Pacientes/Pacientes.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pacientes;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Student
+ */
+public class Pacientes {
+    private int cedula;
+    private String nombre;
+    private Date fecha;
+    private Date HoraLLega;
+    private Date HoraSalida;
+    
+}
